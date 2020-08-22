ef38c1b3d2302b81525dd343d4b7b363cc526538
rocktimsaikia <srocktim61@gmail.com>
2020-08-22 09:54:23 +0530

    style: add prittier for fomatting :zap:

--- app/index.js
+++ app/index.js
@@ -1,53 +1,54 @@
-"use strict";
-const Generator = require("yeoman-generator");
-const dasherized = require("add-dashes");
+'use strict';
+const Generator = require('yeoman-generator');
+const dasherized = require('add-dashes');
 
 class GeneratorGithubAction extends Generator {
 	initializing() {
 		this.defaults = {
 			title: dasherized(this.appname),
 			username: this.user.git.name,
-			usermail: this.user.git.email,
+			usermail: this.user.git.email
 		};
 	}
+
 	prompting() {
 		const prompts = [
 			{
-				type: "input",
-				name: "title",
-				message: "Add a project title ?",
-				default: this.defaults.title,
+				type: 'input',
+				name: 'title',
+				message: 'Add a project title ?',
+				default: this.defaults.title
 			},
 			{
-				type: "input",
-				name: "description",
-				message: "Add a project description ?",
-				default: "My awesome github action",
+				type: 'input',
+				name: 'description',
+				message: 'Add a project description ?',
+				default: 'My awesome github action'
 			},
 			{
-				type: "input",
-				name: "author",
-				message: "Add the authors name?",
-				default: this.defaults.username,
+				type: 'input',
+				name: 'author',
+				message: 'Add the authors name?',
+				default: this.defaults.username
 			},
 			{
-				type: "input",
-				name: "email",
-				message: "Add the authors email ?",
-				default: this.defaults.usermail,
+				type: 'input',
+				name: 'email',
+				message: 'Add the authors email ?',
+				default: this.defaults.usermail
 			},
 			{
-				type: "input",
-				name: "website",
-				message: "Add the authors website url?",
-				default: "https://yourwebsite.com",
+				type: 'input',
+				name: 'website',
+				message: 'Add the authors website url?',
+				default: 'https://yourwebsite.com'
 			},
 			{
-				type: "input",
-				name: "username",
-				message: "Add your Github username?",
-				default: this.defaults.username,
-			},
+				type: 'input',
+				name: 'username',
+				message: 'Add your Github username?',
+				default: this.defaults.username
+			}
 		];
 
 		return this.prompt(prompts).then((answers) => {
@@ -62,36 +63,36 @@ class GeneratorGithubAction extends Generator {
 
 	writingFiles() {
 		this.fs.copy(
-			this.templatePath("_eslintignore"),
-			this.destinationPath(".eslintignore")
+			this.templatePath('_eslintignore'),
+			this.destinationPath('.eslintignore')
 		);
 		this.fs.copy(
-			this.templatePath("_eslintrc.json"),
-			this.destinationPath(".eslintrc.json")
+			this.templatePath('_eslintrc.json'),
+			this.destinationPath('.eslintrc.json')
 		);
 		this.fs.copy(
-			this.templatePath("_gitignore"),
-			this.destinationPath(".gitignore")
+			this.templatePath('_gitignore'),
+			this.destinationPath('.gitignore')
 		);
 		this.fs.copyTpl(
-			this.templatePath("_action.yml"),
-			this.destinationPath("action.yml"),
+			this.templatePath('_action.yml'),
+			this.destinationPath('action.yml'),
 			{
 				appTitle: this.appTitle,
-				appDescription: this.appDescription,
+				appDescription: this.appDescription
 			}
 		);
 		this.fs.copy(
-			this.templatePath("_index.js"),
-			this.destinationPath("index.js")
+			this.templatePath('_index.js'),
+			this.destinationPath('index.js')
 		);
 		this.fs.copy(
-			this.templatePath("_index.test.js"),
-			this.destinationPath("index.test.js")
+			this.templatePath('_index.test.js'),
+			this.destinationPath('index.test.js')
 		);
 		this.fs.copyTpl(
-			this.templatePath("_package.json"),
-			this.destinationPath("package.json"),
+			this.templatePath('_package.json'),
+			this.destinationPath('package.json'),
 			{
 				appTitle: this.appTitle,
 				appDescription: this.appDescription,
@@ -99,55 +100,55 @@ class GeneratorGithubAction extends Generator {
 					name: this.author,
 					email: this.email,
 					website: this.website,
-					username: this.username,
-				},
+					username: this.username
+				}
 			}
 		);
 		this.fs.copyTpl(
-			this.templatePath("_LICENSE"),
-			this.destinationPath("LICENSE"),
+			this.templatePath('_LICENSE'),
+			this.destinationPath('LICENSE'),
 			{
 				author: {
 					name: this.author,
 					email: this.email,
-					website: this.website,
-				},
+					website: this.website
+				}
 			}
 		);
 		this.fs.copyTpl(
-			this.templatePath("_README.md"),
-			this.destinationPath("README.md"),
+			this.templatePath('_README.md'),
+			this.destinationPath('README.md'),
 			{
 				appTitle: this.appTitle,
-				appDescription: this.appDescription,
+				appDescription: this.appDescription
 			}
 		);
 		this.fs.copy(
-			this.templatePath("_wait.js"),
-			this.destinationPath("wait.js")
+			this.templatePath('_wait.js'),
+			this.destinationPath('wait.js')
 		);
 	}
 
 	writingFolder() {
 		this.fs.copyTpl(
-			this.templatePath("_github/_dependabot.yml"),
+			this.templatePath('_github/_dependabot.yml'),
 			this.destinationPath(`.github/dependabot.yml`)
 		);
 		this.fs.copyTpl(
-			this.templatePath("_github/workflows/_test.yml"),
-			this.destinationPath(".github/workflows/test.yml")
+			this.templatePath('_github/workflows/_test.yml'),
+			this.destinationPath('.github/workflows/test.yml')
 		);
 		this.fs.copyTpl(
-			this.templatePath("dist/index.js"),
-			this.destinationPath("dist/index.js")
+			this.templatePath('dist/index.js'),
+			this.destinationPath('dist/index.js')
 		);
 		this.fs.copyTpl(
-			this.templatePath("dist/index.js.map"),
-			this.destinationPath("dist/index.js.map")
+			this.templatePath('dist/index.js.map'),
+			this.destinationPath('dist/index.js.map')
 		);
 		this.fs.copyTpl(
-			this.templatePath("dist/sourcemap-register.js"),
-			this.destinationPath("dist/sourcemap-register.js")
+			this.templatePath('dist/sourcemap-register.js'),
+			this.destinationPath('dist/sourcemap-register.js')
 		);
 	}
 
@@ -155,7 +156,7 @@ class GeneratorGithubAction extends Generator {
 		this.installDependencies({
 			yarn: true,
 			npm: true,
-			bower: false,
+			bower: false
 		});
 	}
 }
